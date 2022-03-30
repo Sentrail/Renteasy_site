0482e002e861eb97bc842788a3f997f6e059dbd9
Israel Yusuf <sentrytim10@gmail.com>
2022-03-30 17:46:10 +0100

    made some changes to the index

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ var myTimeout = setTimeout(function() {
     } else {
     alert("You can't close it, It's opened already");
     }
-    }, 10);
+    }, 0);
 
+function searchForm(){
 var keyWords = "'Boys Quarters', 'boys quarters', 'boys quarter', 'Boys Quarter', 'Bedroom Flat', 'bedroom flat', 'Room', 'room', 'Rooms', 'rooms', 'Room and parlour', 'room and parlour', 'Room Self Contain', 'room self contain', 'Room and Parlour Self Contain', 'room and parlour self contain'";
-function searchForm(formSearch){
 if(formSearch === "Search" || formSearch === ""){
-    document.getElementById('search_box').value="Please Enter a keyword";
+    document.getElementById('searchBox').value;
+    alert("Please Enter a Search keyword");
     return false;
 }
 }
 
 function validateForm() {
-    var Name = document.getElementById("demo").value;
-    if( Name == "") {
-    alert("Please fill in the empty text fields");
+    var phoneNumber = document.getElementById("inputBox4").value;
+    if(confirm( phoneNumber == "!£$%^&*)(_-=}]{[:;@'~#>.,<?/\|")) {
+    alert("Incorrect input in the text field");
     return false;
     }
     }
